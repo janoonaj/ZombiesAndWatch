5c9c6e5256adb30c28a7e1619cf9b622341b508c
jano <jano_alex_es@hotmail.com>
2015-10-12 12:14:41 +0200

    refactor

---
+++ core/src/com/game/buildCityMenu/CityInfoPanel.java
@@ -0,0 +1,39 @@
+package com.game.buildCityMenu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+
+public class CityInfoPanel implements Panel {
+
+    private final CityInfoVO cityInfo;
+    private Table table;
+    private Skin skin;
+
+    public CityInfoPanel(CityInfoVO cityInfo) {
+        this.cityInfo = cityInfo;
+        skin = new Skin(Gdx.files.internal("skins.json"));
+        table = new Table();
+        prepareTable();
+    }
+
+    private void prepareTable() {
+        Label population = new Label("Population: " + cityInfo.getPopulation(), skin);
+        Label maxPopulation = new Label("MaxPopulation: " + cityInfo.getMaxPopulation(), skin);
+        Label food = new Label("Food: " +  cityInfo.getFood(), skin);
+        Label militia = new Label("Militia: Lvl"  +  cityInfo.getMilitiaLevel(), skin);
+        Label defenses = new Label("Defenses: " +  cityInfo.getDefenses() + "%", skin);
+        table.add(population).uniform();
+        table.add(food).uniform();
+        table.add(defenses).uniform();
+        table.row();
+        table.add(maxPopulation).uniform();
+        table.add(militia).uniform();
+    }
+
+    @Override
+    public Table getTable() {
+        return table;
+    }
+}
--- core/src/com/game/buildCityMenu/Menu.java
+++ core/src/com/game/buildCityMenu/Menu.java
@@ -5,173 +5,52 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.game.AssetsFactory;
 
 public class Menu implements Screen {
     private Stage stage;
     private Table table;
-    private Skin skin = new Skin(Gdx.files.internal("skins.json"));
+    private CityInfoPanel cityInfoPanel;
+    private BuildCityController controllerHouse;
+    private BuildCityController controllerHarvest;
+    private BuildCityController controllerWall;
+    private BuildCityController controllerMilitia;
+    private AvailablePointsController availablePointsController;
 
     public Menu(Stage stage) {
         this.stage = stage;
+        cityInfoPanel = new CityInfoPanel(CityInfoVO.test());
+        controllerHouse = new BuildCityController(AssetsFactory.instance().getButtonHouse(), 10);
+        controllerHarvest = new BuildCityController(AssetsFactory.instance().getButtonHarvest(), 10);
+        controllerWall = new BuildCityController(AssetsFactory.instance().getButtonWall(), 10);
+        controllerMilitia = new BuildCityController(AssetsFactory.instance().getButtonMilitia(), 10);
+        availablePointsController = new AvailablePointsController(10);
+        prepareTable();
+
+
+    }
+
+    private void prepareTable() {
         table = new Table();
         table.setFillParent(true);
         table.setDebug(true);
         stage.addActor(table);
-
-
-
-        ///////////
-        // TABLES
-        ///////////
-        Table tableUp = new Table();
         Table tableDown = new Table();
         Table tableDownLeft = new Table().left();
         Table tableDownRight = new Table().right();
-        Table tableButton1 = new Table();
-        Table tableButton2 = new Table();
-        Table tableButton3 = new Table();
-        Table tableButton4 = new Table();
-        tableDownLeft.add(tableButton1).padBottom(10);
-        tableDownLeft.add(tableButton2).padBottom(10);
-
-        Table tableAvailablePoints = new Table();
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                Image mark = new Image(AssetsFactory.instance().getMarkEmpty());
-                mark.setOrigin(mark.getWidth()/2, mark.getHeight()/2);
-                mark.setRotation(90f);
-                tableAvailablePoints.add(mark).padRight(10).padLeft(10);
-            }
-            if(i < 4) tableAvailablePoints.row();
-        }
-        tableDownLeft.add(tableAvailablePoints).padBottom(10);
-
-
+        tableDownLeft.add(controllerHouse.getTable()).padBottom(10);
+        tableDownLeft.add(controllerHarvest.getTable()).padBottom(10);
+        tableDownLeft.add(availablePointsController.getTable()).padBottom(10);
         tableDownLeft.row();
-        tableDownLeft.add(tableButton3);
-        tableDownLeft.add(tableButton4);
-
-
-
+        tableDownLeft.add(controllerWall.getTable());
+        tableDownLeft.add(controllerMilitia.getTable());
         tableDownLeft.add(new ImageButton(new Image(AssetsFactory.instance().getButtonOK()).getDrawable()));
-
-
-
         tableDown.add(tableDownLeft);
         tableDown.add(tableDownRight);
-        table.add(tableUp).padBottom(20);
+        table.add(cityInfoPanel.getTable()).padBottom(20);
         table.row();
         table.add(tableDown);
-
-
-        ///////////
-        // TABLE UP
-        ///////////
-        Label population = new Label("Population: 0", skin);
-        Label maxPopulation = new Label("MaxPopulation: 0", skin);
-        Label food = new Label("Food: 0", skin);
-        Label militia = new Label("Militia: Lvl1", skin);
-        Label defenses = new Label("Defenses: 100%", skin);
-        tableUp.add(population).uniform();
-        tableUp.add(food).uniform();
-        tableUp.add(defenses).uniform();
-        tableUp.row();
-        tableUp.add(maxPopulation).uniform();
-        tableUp.add(militia).uniform();
-
-
-
-
-
-
-        ///////////
-        // TABLE DOWN LEFT
-        ///////////
-
-        ImageButton bttnHouse = new ImageButton(new Image(AssetsFactory.instance().getButtonHouse()).getDrawable());
-        ImageButton bttnHarvest = new ImageButton(new Image(AssetsFactory.instance().getButtonHarvest()).getDrawable());
-        ImageButton bttnWall = new ImageButton(new Image(AssetsFactory.instance().getButtonWall()).getDrawable());
-        ImageButton bttnMilitia = new ImageButton(new Image(AssetsFactory.instance().getButtonMilitia()).getDrawable());
-        ImageButton bttnPlus1 = new ImageButton(new Image(AssetsFactory.instance().getButtonPlus()).getDrawable());
-        ImageButton bttnPlus2 = new ImageButton(new Image(AssetsFactory.instance().getButtonPlus()).getDrawable());
-        ImageButton bttnPlus3 = new ImageButton(new Image(AssetsFactory.instance().getButtonPlus()).getDrawable());
-        ImageButton bttnPlus4 = new ImageButton(new Image(AssetsFactory.instance().getButtonPlus()).getDrawable());
-        ImageButton bttnMinus1 = new ImageButton(new Image(AssetsFactory.instance().getButtonMinus()).getDrawable());
-        ImageButton bttnMinus2 = new ImageButton(new Image(AssetsFactory.instance().getButtonMinus()).getDrawable());
-        ImageButton bttnMinus3 = new ImageButton(new Image(AssetsFactory.instance().getButtonMinus()).getDrawable());
-        ImageButton bttnMinus4 = new ImageButton(new Image(AssetsFactory.instance().getButtonMinus()).getDrawable());
-
-        Table tableSelector1 = new Table();
-        for(int i = 0; i < 10; i++) {
-            tableSelector1.add(new Image(AssetsFactory.instance().getMarkEmpty())).padRight(3);
-        }
-        tableButton1.add(tableSelector1);
-        tableButton1.row();
-        tableButton1.add(bttnHouse);
-        Table tablePluMin1 = new Table();
-        tablePluMin1.add(bttnPlus1);tablePluMin1.row();tablePluMin1.add(bttnMinus1);
-        tableButton1.add(tablePluMin1);
-
-        Table tableSelector2 = new Table();
-        for(int i = 0; i < 10; i++) {
-            tableSelector2.add(new Image(AssetsFactory.instance().getMarkEmpty())).padRight(3);
-        }
-        tableButton2.add(tableSelector2);
-        tableButton2.row();
-        tableButton2.add(bttnHarvest);
-        Table tablePluMin2 = new Table();
-        tablePluMin2.add(bttnPlus2);tablePluMin2.row();tablePluMin2.add(bttnMinus2);
-        tableButton2.add(tablePluMin2);
-
-        Table tableSelector3 = new Table();
-        for(int i = 0; i < 10; i++) {
-            tableSelector3.add(new Image(AssetsFactory.instance().getMarkEmpty())).padRight(3);
-        }
-        tableButton3.add(tableSelector3);
-        tableButton3.row();
-        tableButton3.add(bttnWall);
-        Table tablePluMin3 = new Table();
-        tablePluMin3.add(bttnPlus3);tablePluMin3.row();tablePluMin3.add(bttnMinus3);
-        tableButton3.add(tablePluMin3);
-
-        Table tableSelector4 = new Table();
-        for(int i = 0; i < 10; i++) {
-            tableSelector4.add(new Image(AssetsFactory.instance().getMarkEmpty())).padRight(3);
-        }
-        tableButton4.add(tableSelector4);
-        tableButton4.row();
-        tableButton4.add(bttnMilitia);
-        Table tablePluMin4 = new Table();
-        tablePluMin4.add(bttnPlus4);tablePluMin4.row();tablePluMin4.add(bttnMinus4);
-        tableButton4.add(tablePluMin4);
-
-
-
-
-        ///////////
-        // TABLE DOWN RIGHT
-        ///////////
-        /*Table tableAvailablePoints = new Table();
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
-                Image mark = new Image(AssetsFactory.instance().getMarkEmpty());
-                mark.setOrigin(mark.getWidth()/2, mark.getHeight()/2);
-                mark.setRotation(90f);
-                tableAvailablePoints.add(mark).padRight(10).padLeft(10);
-            }
-            if(i < 4) tableAvailablePoints.row();
-        }
-
-        ImageButton bttnOK = new ImageButton(new Image(AssetsFactory.instance().getButtonOK()).getDrawable());
-        tableDownRight.add(tableAvailablePoints);
-        tableDownRight.row();
-        tableDownRight.add(bttnOK);*/
-
-
     }
 
     @Override
