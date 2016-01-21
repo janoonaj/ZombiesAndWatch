87528448e5da225654d72704590deb5a1965414d
jano <jano_alex_es@hotmail.com>
2016-01-21 12:55:15 +0100

    Icons on City upper panel

--- core/src/com/game/buildCityMenu/CityInfoPanel.java
+++ core/src/com/game/buildCityMenu/CityInfoPanel.java
@@ -1,9 +1,11 @@
 package com.game.buildCityMenu;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.game.AssetsFactory;
 import com.game.CityInfoVO;
 
 public class CityInfoPanel implements Panel {
@@ -20,17 +22,23 @@ public class CityInfoPanel implements Panel {
     }
 
     private void prepareTable() {
-        Label population = new Label("Population: " + cityInfo.getPopulation(), skin);
-        Label maxPopulation = new Label("MaxPopulation: " + cityInfo.getMaxPopulation(), skin);
-        Label food = new Label("Food: " +  cityInfo.getFood(), skin);
-        Label militia = new Label("Militia: Lvl"  +  cityInfo.getMilitiaPoints(), skin);
-        Label defenses = new Label("Defenses: " +  cityInfo.getDefenses() + "%", skin);
+        Image population = new Image(AssetsFactory.instance().getIconPopulation());
+        Image maxPopulation = new Image(AssetsFactory.instance().getIconHouse());
+        Image food = new Image(AssetsFactory.instance().getIconHarvest());
+        Image militia = new Image(AssetsFactory.instance().getIconMilitia());
+        Image defenses = new Image(AssetsFactory.instance().getIconWall());
+
         table.add(population).uniform();
+        table.add(new Label(cityInfo.getPopulation() + "", skin)).uniform();
         table.add(food).uniform();
+        table.add(new Label(cityInfo.getFood() + "", skin)).uniform();
         table.add(defenses).uniform();
-        table.row();
+        table.add(new Label(cityInfo.getDefenses() + "%", skin)).uniform();
+        table.row();;
         table.add(maxPopulation).uniform();
+        table.add(new Label(cityInfo.getMaxPopulation() + "", skin)).center().uniform();
         table.add(militia).uniform();
+        table.add(new Label("lvl" + cityInfo.getMilitiaLevel(), skin)).center();
     }
 
     @Override
